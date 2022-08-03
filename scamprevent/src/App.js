7ecdd499970916b9847ef2c974254630b911b514
Emil <emilcc89@gmail.com>
2022-08-03 14:22:00 +0200

    Created Landing page

--- scamprevent/src/App.js
+++ scamprevent/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
 
-function App() {
+//pages
+import WelcomePage from './Pages/WelcomePage';
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
---
+++ scamprevent/src/Pages/WelcomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+const WelcomePage = () => {
+
+    return (
+        <div className='WelcomePage'>
+           <h1>Hi</h1>
+        </div>
+    );
+}
+
+export default WelcomePage
